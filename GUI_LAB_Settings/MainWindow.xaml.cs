7a232fa7e98ec4039fa11859d24d9d0ae17bcfc1
Bertram <bw-sp@live.dk>
2019-09-26 13:49:21 +0200

    Add project files.

---
+++ GUI_LAB_Settings/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_LAB_Settings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Properties.Settings.Default.Save();
+        }
+
+        public void SaveSettings()
+        {
+            Properties.Settings.Default.Age = Convert.ToInt32(TbAge.Text);
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSettings();
+        }
+    }
+}
